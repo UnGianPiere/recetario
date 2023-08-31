18f4947c34043d83bce299959275a267ba87e238
pier <kknero@gmail.com>
2023-08-31 18:17:11 -0500

    cambio

---
+++ main.py
@@ -0,0 +1,18 @@
+from builtins import print
+
+a=2
+b=5
+def suma(a,b):
+    c=a+b
+    return c
+list1=[4,3,5,6]
+for i in range(0,4):
+    print(list1[i])
+
+print(len(list1))
+c=suma(4,5)
+print(c)
+
+
+
+
